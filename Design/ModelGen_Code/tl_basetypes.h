46aa96172822f6e33d353e5b7b5b4a15cd5b4057
Andy R <xnorlogic@gmail.com>
2019-08-17 11:46:09 -0400

    Initial Commit

---
+++ Design/ModelGen_Code/tl_basetypes.h
@@ -0,0 +1,31 @@
+/**************************************************************************************************\
+ *** 
+ *** Simulink model       : TestModel1
+ *** TargetLink subsystem : TestModel1/TesModel1
+ *** Codefile             : tl_basetypes.h
+ ***
+ *** Generation date: 2019-07-27 20:44:51
+ ***
+ *** TargetLink version      : 4.2p2 from 27-Nov-2017
+ *** Code generator version  : Build Id 4.2.0.33 from 2017-11-28 14:39:24
+
+\**************************************************************************************************/
+
+#ifndef TL_BASETYPES_H
+#define TL_BASETYPES_H
+
+
+typedef unsigned char Bool; /* Description: boolean basetype */
+typedef float Float32; /* Description: 32 bit floating-point basetype */
+typedef double Float64; /* Description: 64 bit floating-point basetype */
+typedef signed short int Int16; /* Description: 16 bit signed integer basetype */
+typedef signed long int Int32; /* Description: 32 bit signed integer basetype */
+typedef signed char Int8; /* Description: 8 bit signed integer basetype */
+typedef unsigned short int UInt16; /* Description: 16 bit unsigned integer basetype */
+typedef unsigned long int UInt32; /* Description: 32 bit unsigned integer basetype */
+typedef unsigned char UInt8; /* Description: 8 bit unsigned integer basetype */
+
+#endif /* TL_BASETYPES_H */
+/*----------------------------------------------------------------------------*\
+  END OF FILE
+\*----------------------------------------------------------------------------*/
